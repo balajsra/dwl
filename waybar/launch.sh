81bb55f1857227b286c0d8ecfee015ed54f0888e
Sravan Balaji <balajsra@umich.edu>
2024-12-29 13:46:32 -0500

    Idle Inhibitor Active on Startup
    - Make killall waybar fail silently if waybar
      isn't already running
    - Update launch script to run in foreground
    - Use verbose version of waybar CLI args
    - Automatically restart waybar if it exits
      with non 143 (SIGTERM) exit code so random
      crashes (e.g., when opening LibreOffice or
      some games) don't persist and bar gets
      restarted

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
-killall waybar
+killall waybar || true
 
-/usr/bin/waybar \
-    -c $HOME/.config/dwl/waybar/config.jsonc \
-    -s $HOME/.config/dwl/waybar/style.css &
+until /usr/bin/waybar \
+    --config $HOME/.config/dwl/waybar/config.jsonc \
+    --style $HOME/.config/dwl/waybar/style.css;
+do
+    if [ $? -ne 143 ]; then
+        echo "Dwl Waybar stopped with exit code $?. Respawning..." >&2;
+        sleep 1;
+    else
+        echo "Dwl Waybar manually killed with SIGTERM";
+        break;
+    fi
+done
